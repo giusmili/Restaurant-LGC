1e629537ca965787bd3785ca7f9cf7b415137997
giusmili <giusmili67@gmail.com>
2025-09-27 11:20:49 +0200

    style: format code for consistency and readability in sw.js

--- sw.js
+++ sw.js
@@ -5,27 +5,28 @@ const URLS_TO_CACHE = [
   "/Restaurant-LGC/menu.html",
   "/Restaurant-LGC/styles.css",
   "/Restaurant-LGC/script.js",
-  "/Restaurant-LGC/offline.html"
+  "/Restaurant-LGC/offline.html",
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_NAME).then((cache) => {
       return Promise.all(
-        URLS_TO_CACHE.map(url =>
+        URLS_TO_CACHE.map((url) =>
           fetch(url)
-            .then(resp => {
+            .then((resp) => {
               if (!resp.ok) throw new Error(`Échec chargement ${url}`);
               return cache.put(url, resp);
             })
-            .catch(err => console.warn("⚠️ Non ajouté au cache :", url, err))
+            .catch((err) => console.warn("⚠️ Non ajouté au cache :", url, err))
         )
       );
     })
   );
 });
-  if ("serviceWorker" in navigator) {
-            navigator.serviceWorker.register("/Restaurant-LGC/sw.js")
-                .then(() => console.log("Service Worker enregistré !"))
-                .catch(err => console.error("SW échec :", err));
-            }
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/Restaurant-LGC/sw.js")
+    .then(() => console.log("Service Worker enregistré !"))
+    .catch((err) => console.error("SW échec :", err));
+}
